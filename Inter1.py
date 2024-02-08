1c26fd98de94d1d3ca7f9469b98fbe63534bfe96
lucas-bendia <lucas.bendia@laplateforme.io>
2024-02-08 15:31:10 +0100

    fenetre 1 fait

--- Inter1.py
+++ Inter1.py
@@ -29,22 +29,22 @@ def create_gui():
     bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
     # Créer les champs de saisie pour l'identifiant et le mot de passe
-    identifiant_entry = ctk.CTkEntry(root, width=250)
+    identifiant_entry = ctk.CTkEntry(root, width=250, justify="center")
     identifiant_entry.place(x=640, y=390, anchor='center')
-    identifiant_entry.configure(placeholder_text="Identifiant", fg_color="black",text_color="#9489ae",placeholder_text_color="#9489ae", corner_radius=0)
+    identifiant_entry.configure(placeholder_text="Identifiant", fg_color="black",text_color="#9489ae",placeholder_text_color="#9489ae", corner_radius=0, font=("Gill Sans MTç", 12))
 
 
 
-    mot_de_passe_entry = ctk.CTkEntry(root, width=250, show='*')
+    mot_de_passe_entry = ctk.CTkEntry(root, width=250, show='*', justify="center")
     mot_de_passe_entry.place(x=640, y=450, anchor='center')
-    mot_de_passe_entry.configure(placeholder_text="Mot de passe", fg_color="black",text_color="#9489ae",placeholder_text_color="#9489ae", corner_radius=0)
+    mot_de_passe_entry.configure(placeholder_text="Mot de passe", fg_color="black",text_color="#9489ae",placeholder_text_color="#9489ae", corner_radius=0, font=("Gill Sans MT", 12))
 
     # Ajouter un label "Pas encore inscrit ?"
-    inscription_label = tk.Label(root, text="Pas encore inscrit ?", bg='#9489ae', fg='#2d243f')
+    inscription_label = tk.Label(root, text="Pas encore inscrit ?", bg='#9489ae', fg='#2d243f', font=("Gill Sans MT", 12))
     inscription_label.place(x=640, y=490, anchor='center')
 
     # Ajouter un bouton pour ouvrir la page d'inscription
-    inscription_button = ctk.CTkButton(root, text="Inscription", command=open_inscription, fg_color='#2d243f',text_color="#9489ae")
+    inscription_button = ctk.CTkButton(root, text="Inscription", command=open_inscription, fg_color='#2d243f',text_color="#9489ae", font=("Gill Sans MT", 18))
     inscription_button.place(x=640, y=520, anchor='center')
 
     root.mainloop()
