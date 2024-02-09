a0d22cd69f3d8d23f978997c545d115a867d78cd
marion-borne <marion.borne@laplateforme.io>
2024-02-09 10:45:23 +0100

    page inscription finie

--- inscription.py
+++ inscription.py
@@ -8,6 +8,13 @@ def clear_entry(event, entry):
 def open_inscription():
     # Importer le contenu de inscription.py
     import inscription
+    
+# Modifiez la signature de la fonction pour accepter l'entry comme argument
+def toggle_password_visibility(entry):
+    if entry.cget('show') == '*':
+        entry.configure(show='')
+    else:
+        entry.configure(show='*')
 
 def create_gui():
     root = tk.Tk()
@@ -17,13 +24,10 @@ def create_gui():
 
     # Charger l'image de fond
     image = Image.open("images/page2.png")
-
     # Redimensionner l'image
-    image = image.resize((1280, 720), Image.LANCZOS)
-
+    image = image.resize((1280, 720), Image.Resampling.LANCZOS)
     # Convertir l'image PIL en image Tkinter
     bg_image = ImageTk.PhotoImage(image)
-
     # Créer un label pour afficher l'image de fond
     bg_label = tk.Label(root, image=bg_image)
     bg_label.place(x=0, y=0, relwidth=1, relheight=1)
@@ -47,15 +51,26 @@ def create_gui():
     email_entry.configure(placeholder_text="E-mail", fg_color="black",text_color="#9489ae",placeholder_text_color="#9489ae", corner_radius=0)
 
     # Créer les champs de saisie pour le mot de passe
-    mot_de_passe_entry = ctk.CTkEntry(root, width=250, show='*', font=custom_font, justify='center')
+    mot_de_passe_entry = ctk.CTkEntry(root, width=250, show='*', font=("Gill Sans MT", 14), justify='center')
     mot_de_passe_entry.place(x=640, y=480, anchor='center')
     mot_de_passe_entry.configure(placeholder_text="Mot de passe", fg_color="black",text_color="#9489ae",placeholder_text_color="#9489ae", corner_radius=0)
 
+    # Charger et redimensionner l'image de l'œil
+    eye_img = Image.open("images/oeil.png")
+    eye_img.thumbnail((eye_img.width, 13), Image.Resampling.LANCZOS)
+    eye_open_image = ImageTk.PhotoImage(eye_img)
+
+    # Créer un bouton pour afficher/masquer le mot de passe
+    # Utilisez une fonction lambda pour passer mot_de_passe_entry en argument
+    eye_btn = tk.Button(root, image=eye_open_image, command=lambda: toggle_password_visibility(mot_de_passe_entry), borderwidth=0, bg='black')
+    eye_btn.image = eye_open_image  # Gardez une référence de l'image pour éviter le ramasse-miettes
+    eye_btn.place(x=727, y=473)  # Ajustez si nécessaire pour l'emplacement exact
+     
     # Ajouter un bouton pour Continuer
     inscription_button = ctk.CTkButton(root, text="Continuer", command=open_inscription, fg_color='#2d243f',text_color="#9489ae")
     inscription_button.place(x=640, y=620, anchor='center')
     
-   # Créer un Canvas pour la croix à l'emplacement désiré
+    # Créer un Canvas pour la croix à l'emplacement désiré
     canvas_cross = tk.Canvas(root, width=24, height=24, bg='#2d243f', highlightthickness=0)
     canvas_cross.place(x=423, y=543)  # Ajustez si nécessaire pour l'emplacement exact
 
