89558ea9ad38e3caea07baf2586b6cf638e3f113
ilian-bonsens <ilian.bonsens@laplateforme.io>
2024-02-13 11:37:28 +0100

    interface finie 1.0 finie

--- home4.py
+++ home4.py
@@ -15,16 +15,30 @@ def create_gui():
 
     # Charger l'image de fond
     image = Image.open("images/home.png")
+    button_image = Image.open("images/add.png")
 
     # Redimensionner l'image
     image = image.resize((1280, 720), Image.LANCZOS)
+    button_image = button_image.resize((55, 55), Image.LANCZOS)
 
     # Convertir l'image PIL en image Tkinter
     bg_image = ImageTk.PhotoImage(image)
+    tk_image = ImageTk.PhotoImage(button_image)
 
     # Créer un label pour afficher l'image de fond
     bg_label = tk.Label(root, image=bg_image)
     bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    # Créer un bouton avec l'image du bouton
+    button_friends = tk.Button(root, image=tk_image, borderwidth=0, highlightthickness=0)
+    button_friends.image = tk_image  # Gardez une référence à l'image
+    button_friends.place(x=255, y=140, anchor='nw')  # Modifiez les coordonnées x et y si nécessaire
+    button_friends.configure(bg='#2d243f')
+
+    button_groups = tk.Button(root, image=tk_image, borderwidth=0, highlightthickness=0)
+    button_groups.image = tk_image  # Gardez une référence à l'image
+    button_groups.place(x=20, y=140, anchor='nw')  # Modifiez les coordonnées x et y si nécessaire
+    button_groups.configure(bg='#2d243f')
 
     #Créer une frame qui contiendra les widgets de la conversation
     frame = ctk.CTkFrame(root)
