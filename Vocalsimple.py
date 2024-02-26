41a90e3f6823b70ccc6799d472647b72a3ca7c7e
marion-borne <marion.borne@laplateforme.io>
2024-02-26 12:03:24 +0100

    compression du binaire dans bdd

--- Vocalsimple.py
+++ Vocalsimple.py
@@ -2,6 +2,8 @@ import mysql.connector
 from datetime import datetime
 import sounddevice as sd
 import wavio
+import gzip
+import traceback
 
 # Configuration de la base de données
 db_config = {
@@ -10,30 +12,35 @@ db_config = {
     'host': 'localhost',
     'database': 'Discord',
     'raise_on_warnings': True,
-    'use_pure': True,  # Utilisez le pilote Python pur plutôt que le pilote C
+    'use_pure': True,
     'autocommit': True,
-    'charset': 'utf8mb4',  # Définissez explicitement le charset
+    'charset': 'utf8mb4',
     'collation': 'utf8mb4_general_ci',
 }
 
-# Fonction pour sauvegarder l'audio dans la base de données
-def save_audio_to_db(config, audio_file_path):
+# Fonction pour compresser l'audio
+def compress_audio(audio_data):
+    return gzip.compress(audio_data)
+
+# Fonction pour sauvegarder l'audio compressé dans la base de données
+def save_compressed_audio_to_db(config, audio_data):
     # Connectez-vous à la base de données
     cnx = mysql.connector.connect(**config)
     c = cnx.cursor()
 
-    # Ouvrez le fichier audio en mode binaire
-    with open(audio_file_path, 'rb') as audio_file:
-        audio_data = audio_file.read()
+    # Comprimez les données audio
+    compressed_audio_data = compress_audio(audio_data)
 
     # Obtenez l'heure actuelle
     now = datetime.now()
 
-    # Insérez les données audio et la date/heure dans la base de données
+    # Insérez l'audio compressé et la date/heure dans la base de données
     try:
-        c.execute("INSERT INTO messages (audio, date_heure) VALUES (%s, %s)", (audio_data, now))
-    except mysql.connector.Error as err:
+        c.execute("INSERT INTO messages (audio, date_heure) VALUES (%s, %s)", (compressed_audio_data, now))
+        cnx.commit()  # Commit explicitement, au cas où l'autocommit ne fonctionnerait pas
+    except Exception as err:
         print(f"Something went wrong: {err}")
+        traceback.print_exc()  # Cela affichera la trace complète de l'erreur
     finally:
         c.close()
         cnx.close()
@@ -52,7 +59,11 @@ print("Recording finished.")
 audio_filename = 'output.wav'
 wavio.write(audio_filename, myrecording, fs, sampwidth=2)
 
-# Sauvegardez l'audio dans la base de données
-save_audio_to_db(db_config, audio_filename)
+# Ouvrez le fichier audio en mode binaire et lisez-le
+with open(audio_filename, 'rb') as audio_file:
+    audio_data = audio_file.read()
 
-print("Audio saved to database.")
+# Sauvegardez l'audio compressé dans la base de données
+save_compressed_audio_to_db(db_config, audio_data)
+
+print("Compressed audio saved to database.")
